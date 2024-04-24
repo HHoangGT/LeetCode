3c0bddc3650322630de39c0ebf721b5aea600aad
HoangNH <hoang.dasher@gmail.com>
2024-04-24 23:06:53 +0700

    finish problem 443

--- PythonQuestions/Leetcode/main.py
+++ PythonQuestions/Leetcode/main.py
@@ -81,6 +81,42 @@ def productExceptSelf(nums: list[int]) -> list[int]:
         return [0 for num in nums]
 
 
+# Problem 443 LeetCode
+def compress(chars: list[str]) -> int:
+    res = ""
+    count = 0
+    index = 0
+    if len(chars) == 1:
+        return 1
+    while index < len(chars):
+        if index == 0:
+            count += 1
+            index += 1
+            continue
+        else:
+            if chars[index] == chars[index - 1]:
+                count += 1
+                index += 1
+                if (index == len(chars)):
+                    res += chars[index - 1]
+                    if count != 1:
+                        res += str(count)
+                continue
+            else:
+                res += chars[index - 1]
+                if count != 1:
+                    res += str(count)
+                count = 1
+                index += 1
+                if (index == len(chars)):
+                    res += chars[index - 1]
+                    if count != 1:
+                        res += str(count)
+    chars[:] = list(res)
+    return len(chars)
+
+
 if __name__ == '__main__':
-    nums = [-1, 1, 0, -3, 3]
-    print(productExceptSelf(nums=nums))
+    chars = ["a", "b", "c"]
+    print(compress(chars))
+    print(chars)
